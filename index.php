e9ca2d10ee09d54b95883f524628355914c6bb39
Maxim <bmw_077@mail.ru>
2022-09-01 23:06:47 +0300

    start

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<form action="" method="post">
+    <input type="text" name="title">
+    <input type="text" name="description">
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
